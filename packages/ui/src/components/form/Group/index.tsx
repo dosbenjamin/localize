fd7a79afe879b4e6bd289fca00d9022cc1499e79
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-12-23 19:51:11 +0100

    refactor(ui): remove form group container component

--- packages/ui/src/components/form/Group/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { PropsWithChildren } from 'react'
-
-type GroupProps = PropsWithChildren
-
-export const Group = (props: GroupProps) => <div className="flex flex-col space-y-2" {...props} />
--- packages/ui/src/components/form/index.ts
+++ packages/ui/src/components/form/index.ts
@@ -1,3 +1,2 @@
-export { Group } from './Group'
 export { Input } from './Input'
 export { Label } from './Label'
