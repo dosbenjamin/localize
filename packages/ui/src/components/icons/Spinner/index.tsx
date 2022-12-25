8ae7993efc76b9244abdba09f425e56f4f0356bc
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-12-25 18:03:06 +0100

    fix(ui): spinner stroke-width not in camelCase

--- packages/ui/src/components/icons/Spinner/index.tsx
+++ packages/ui/src/components/icons/Spinner/index.tsx
@@ -5,7 +5,7 @@ export type SpinnerProps = ComponentPropsWithoutRef<'svg'>
 
 export const Spinner = ({ className, ...props }: SpinnerProps) => (
   <svg className={cx('animate-spin', className)} fill="none" viewBox="0 0 24 24" {...props}>
-    <circle opacity="0.25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    <circle opacity="0.25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
     <path
       opacity="0.75"
       fill="currentColor"
