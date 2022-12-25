a951829b20c7d91f4ad3d94e5257167c1b133bab
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-12-25 18:44:29 +0100

    feat(ui): add button loading state

--- packages/ui/src/components/Button/index.tsx
+++ packages/ui/src/components/Button/index.tsx
@@ -1,13 +1,20 @@
 import { cva, type VariantProps } from 'class-variance-authority'
-import type { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { Spinner } from '../icons'
 
 const buttonVariants = cva('uppercase text-center outline-none', {
   variants: {
     intent: {
-      primary: 'bg-orange text-white',
+      primary: 'bg-orange text-white disabled:brightness-50',
     },
     size: {
       medium: 'text-md px-8 py-4 variation-wght-600',
+    },
+    disabled: {
+      true: 'cursor-not-allowed',
+    },
+    loading: {
+      true: 'flex items-center justify-center',
     },
   },
   defaultVariants: {
@@ -16,8 +23,15 @@ const buttonVariants = cva('uppercase text-center outline-none', {
   },
 })
 
-type ButtonProps = ComponentPropsWithoutRef<'button'> & VariantProps<typeof buttonVariants>
+type ButtonProps = ComponentPropsWithoutRef<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    loading?: boolean
+    loadingMessage?: ReactNode
+  }
 
-export const Button = ({ intent, size, ...props }: ButtonProps) => (
-  <button className={buttonVariants({ intent, size })} {...props} />
+export const Button = ({ children, disabled, intent, loading, loadingMessage, size, ...props }: ButtonProps) => (
+  <button disabled={disabled} className={buttonVariants({ disabled, loading, intent, size })} {...props}>
+    {loading && loadingMessage ? loadingMessage : children}
+    {loading && <Spinner className="ml-4 w-4" />}
+  </button>
 )
