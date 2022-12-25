5fa8653f56cf526e538fa7d3f093bf29dfc454c7
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-12-25 21:15:04 +0100

    refactor(web): improve toast mapper typing

--- apps/web/components/Toast.tsx
+++ apps/web/components/Toast.tsx
@@ -1,8 +1,7 @@
 import { Notification, type NotificationProps } from '@localize/ui'
-import type { FunctionComponent } from 'react'
 import type { ToastType } from 'react-hot-toast/headless'
 
-const toasts: Record<ToastType, FunctionComponent<NotificationProps>> = {
+const toasts: Record<ToastType, (props: NotificationProps) => JSX.Element> = {
   blank: (props) => <Notification {...props} />,
   custom: ({ children }) => <>{children}</>,
   error: (props) => <Notification intent="error" {...props} />,
